24c2f34b2a4b2863dd0acba60c1844ed444e1a09
Praveen <praveensingam4@gmail.com>
2024-05-10 21:42:13 +0530

    Add all files to repository

---
+++ src/main/java/com/sp/util/AppConstants.java
@@ -0,0 +1,13 @@
+package com.sp.util;
+
+public class AppConstants {
+	//AppConstants can be used to store application-wide constants such as 
+	//URLs, default values, error messages, etc.
+	
+	//Here we are using it use store string literals.
+	
+	public static final String WELCOME_MSG_KEY = "wlcmMessage";
+	
+	public static final String GREET_MSG_KEY = "greetMessage";
+
+}
