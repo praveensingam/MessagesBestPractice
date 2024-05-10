e1088499df68ef7d6ffcbfcce3bac00ce758ac31
Praveen <praveensingam4@gmail.com>
2024-05-10 18:40:53 +0530

    first commit

---
+++ src/main/java/com/sp/rest/BasicController.java
@@ -0,0 +1,29 @@
+package com.sp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicController {//We use this approach when we have less no of messages to be declared.
+
+	@Value("${quote}")
+	String quotation;
+	
+	@Value("${advice}")
+	String advice;
+	
+	@GetMapping("/quotations")
+	public String quotes() {
+		
+		return quotation;
+		
+	}
+	
+	@GetMapping("/advice")
+	public String advices() {
+		
+		return advice;
+	}
+
+}
---
+++ src/main/java/com/sp/rest/MessageController.java
@@ -0,0 +1,35 @@
+package com.sp.rest;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sp.util.AppConstants;
+import com.sp.util.AppProperties;
+
+@RestController
+public class MessageController {// We use this approach when we have lot of messages to be declared.
+
+	@Autowired
+	private AppProperties appProperties;
+
+	@GetMapping("/welcome")
+	public String welcome() {
+
+//		Map<String, String> messages = appProperties.getMessages();
+//		
+//		return messages.get("wlcmMessage");
+
+		return appProperties.getMessages().get(AppConstants.WELCOME_MSG_KEY); // To avoid hard coding string literals
+																				// with help of AppConstants
+	}
+
+	@GetMapping("/greet")
+	public String greet() {
+
+		return appProperties.getMessages().get(AppConstants.GREET_MSG_KEY);
+	}
+
+}
