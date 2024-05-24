e6e166b389d94dc260311f059760dcd5ddaa115f
Praveen <praveensingam4@gmail.com>
2024-05-24 12:34:11 +0530

    yml file modified

---
+++ src/main/java/com/sp/MessagesBestPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagesBestPracticeApplication{
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MessagesBestPracticeApplication.class, args);
+	}
+
+}
--- src/main/java/com/sp/util/AppConstants.java
+++ src/main/java/com/sp/util/AppConstants.java
@@ -4,7 +4,7 @@ public class AppConstants {
 	//AppConstants can be used to store application-wide constants such as 
 	//URLs, default values, error messages, etc.
 	
-	//Here we are using it use store string literals.
+	//Here we are using it to store string literals.
 	
 	public static final String WELCOME_MSG_KEY = "wlcmMessage";
 	
